78b703a01ed943383bd8dba747d8de7b064562f8
Sam Kome <sam.kome@cuc.claremont.edu>
2016-07-09 14:58:12 -0700

    Added hit rate to status message

--- cabbie.py
+++ cabbie.py
@@ -1,18 +1,17 @@
 #! /usr/bin/python
-import os,sys,time
-import urllib2
+import os,sys
 from worldcat.request.search import SRURequest
 from worldcat.util.extract import extract_elements, pymarc_extract
 import csv
 
 #Ex: Whole list of LINK+ libraries
 #LIBS = "ALLIU,CAP,CBC,CCO,CDS,CFS,CFV,CLO,CLV,CMM,CPO,CPS,CPT,CPU,CRP,CSF,CSH,CSJ,CSO,CTU,CUF,CWC,GH0,HAY,JQA,JQF,JQJ,JRR,JRS,JRZ,LIV,LLU,LML,LMR,MIS,MVP,NNY,PAP,PN#,PP2,QP9,SDG,SFR,SJP,STA,SVW,SXP,UNL,UOA"
-LIBS = "HDC"
+LIBS = "HDC" #empty string means ALL libraries
 WSKEY = 'EOdqmdJaycDuD5oEdJgAei89D1qEfmY4xS7iAah4arZYUWhHP4KVP7i6C5Dg4YYegw04dgYLn0mB6LqH'
 SVCLVL = 'full'
 SRUELEM = 'srw.bn'#ISBN
 #SRUELEM = 'srw.no'#OCLC Num 
-MARCCODES = ['001','020','050','245']
+MARCCODES = ['001','020','050','245','264']
 doc=""" 
 Usage: %prog [inputfile] [outputfile]]
 """
@@ -42,7 +41,7 @@ def makeItemList(pymarcResults, lib, lCode):
                try:
                    lItem.append(r[rec].format_field().encode('utf-8'))
                except AttributeError:
-                   lItem.append('{}=n/a'.format(rec))
+                   lItem.append('{} = n/a'.format(rec))
     except Exception as e:
         print "Error: ",e
     return lItem
@@ -51,9 +50,11 @@ def search(lCodes):
     lcabsHeld = []
     lcabsNotHeld = []
     numCodes = 0
+    heldRate = 0.0
     #loop through the list of book codes, search WC, write results to output file
     for numCodes, lCode in enumerate(lCodes):
-        print ('{} / {}\r'.format(numCodes, len(lCodes))),
+        heldRate = float(len(lcabsHeld))/len(lCodes)
+        print ('Record: {} / {} Held Rate = {:.2f}\r'.format(numCodes, len(lCodes), heldRate)),
         sys.stdout.flush()
         query = '({}="{}") and (srw.li="{}")'.format(SRUELEM,lCode,LIBS) #Ex: sru.args['query'] = '(srw.no="122347155") and (srw.li="HDC")'
         sru.args['query'] = query # set the query
