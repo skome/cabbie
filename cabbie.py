8542338661c2d0dc2622cfee811536dd05cc901b
Sam Kome <sam.kome@cuc.claremont.edu>
2016-07-10 15:09:44 -0700

    added YAML routine

--- cabbie.py
+++ cabbie.py
@@ -4,19 +4,11 @@ from worldcat.request.search import SRURequest
 from worldcat.util.extract import extract_elements, pymarc_extract
 import csv
 import yaml
-with open("cabbie.cfg", 'r') as ymlfile:
-    config = yaml.load(ymlfile)
-for section in config:
-    print section
-WSKEY = config['Auth']['WSKEY']
-LIBS = config['Config']['LIBS']
-SVCLVL = config['Config']['SVCLVL']
-SRUELEM = config['Config']['SRUELEM']
-MARCCODES = config['Config']['MARCCODES']
 
 doc=""" 
 Usage: %prog [inputfile] [outputfile]]
 """
+cfgFileName = 'cabbie.cfg'
 
 class codesList: #read in file, de-dupe
     def __init__(self, fh):
@@ -28,6 +20,11 @@ class codesList: #read in file, de-dupe
         lines = list(set(lines)) #De-dupe the list
         return lines
 
+def getYAMLConfig(fname):
+    with open(fname,'r') as ymlf:
+        config = yaml.load(ymlf)
+    return config
+    
 def makeItemList(pymarcResults, lib, lCode):
     lItem = []
     try:#Extract the fields we want from the result set. Some fields we want may be unavailable 
@@ -48,6 +45,7 @@ def search(lCodes):
     lcabsNotHeld = []
     numCodes = 0
     heldRate = 0.0
+
     #loop through the list of book codes, search WC, write results to output file
     for numCodes, lCode in enumerate(lCodes):
         heldRate = float(len(lcabsHeld))/len(lCodes)
@@ -56,18 +54,24 @@ def search(lCodes):
         query = '({}="{}") and (srw.li="{}")'.format(SRUELEM,lCode,LIBS) #Ex: sru.args['query'] = '(srw.no="122347155") and (srw.li="HDC")'
         sru.args['query'] = query # set the query
         results = pymarc_extract(sru.get_response().data) # send the query, extract the results from MARC into list
-        if len(results) ==0:
+        if len(results) ==0: #Honnold has no holdings, open the search
             libs='ALL'
             query = '({}="{}")'.format(SRUELEM,lCode) #no library code; search all libs
             sru.args['query'] = query # set the query
             results = pymarc_extract(sru.get_response().data) # send the query, extract the results from MARC into list          
             lcabsNotHeld.append(makeItemList(results,libs,lCode))
-        else:
+        else: #Check Honnold holdings
             libs = LIBS
             lcabsHeld.append(makeItemList(results,libs,lCode))
     return lcabsHeld, lcabsNotHeld
  
 if __name__ == "__main__":
+    config = getYAMLConfig(cfgFileName)
+    WSKEY = config['Auth']['WSKEY']
+    LIBS = config['Config']['LIBS']
+    SVCLVL = config['Config']['SVCLVL']
+    SRUELEM = config['Config']['SRUELEM']
+    MARCCODES = config['Config']['MARCCODES']
     lCodes=[]
     # Set up the worldcat SRU request 
     sru = SRURequest(wskey=WSKEY)
@@ -79,7 +83,7 @@ if __name__ == "__main__":
     with open(fileIn, 'r') as cabCodes , open(fileOut, 'w') as bibsOut:
         csvOut = csv.writer(bibsOut, quoting=csv.QUOTE_NONNUMERIC)
         csvOut.writerow(csvHdr)
-        bList = codesList(cabCodes) #Returns a de-duped list, this is why results file will be smaller than source file
+        bList = codesList(cabCodes) #Returns a de-duped list; results file may be smaller than source file
         lCodes = bList.listed()
         matches, nonmatches = search(lCodes) # look them all up, return worldcat bib data 
         print('{} matches and {} nonmatches, {:.1f}% match rate'.format(len(matches), len(nonmatches),float(len(matches))/len(lCodes)*100))
